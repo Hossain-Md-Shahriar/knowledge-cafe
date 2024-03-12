b2cd23769cf5d9bfb65cd69bdab261f16b1f300e
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-03-12 21:28:13 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Header from "./components/Header"
+import Blogs from "./components/Blogs";
+import Bookmarks from "./components/Bookmarks";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <div className="container mx-auto">
+        <div className="mx-4">
+          <Header />
+          <div className="md:flex">
+            <Blogs />
+            <Bookmarks />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
